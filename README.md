fdeae43d99404ab88a9ad7062190978903c274ef
Aron Atif <aronatif@360gmail.com>
2025-09-14 21:54:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A simple async echo server in rust using tokio
+### The netcat.exe is there for running on windows as it does not have netcat by default
+### The source code is [here](https://github.com/Aron-Aitf/netcat-rust)
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![warn(
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo
+)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::cargo_common_metadata,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::unwrap_used,
+    clippy::single_call_fn,
+    clippy::allow_attributes_without_reason,
+    clippy::infinite_loop,
+    clippy::allow_attributes
+)]
+
+use tokio::{
+    io::{AsyncReadExt as _, AsyncWriteExt as _},
+    net::{TcpListener, TcpStream},
+    spawn,
+};
+
+const HOST: &str = "localhost";
+const PORT: &str = "8000";
+const PORT_SEPARATOR: &str = ":";
+#[tokio::main(flavor = "multi_thread")]
+async fn main() {
+    let tcp_url = format!("{HOST}{PORT_SEPARATOR}{PORT}");
+    let listener = TcpListener::bind(tcp_url).await.unwrap();
+    loop {
+        #[allow(unused_mut)]
+        let (mut socket, _ip) = listener.accept().await.unwrap();
+        spawn(handle_connection(socket));
+    }
+}
+
+async fn handle_connection(mut socket: TcpStream) {
+    loop {
+        let mut buffer = String::new();
+        match socket.read_to_string(&mut buffer).await {
+            Ok(0) | Err(_) => break,
+            Ok(n) => n,
+        };
+        socket.write_all(buffer.as_bytes()).await.unwrap();
+    }
+}
